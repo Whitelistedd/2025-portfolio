90d96b20d26fd6eeaf4987c3e2fdfc8842dca529
Whitelistedd <geforcexxy@gmail.com>
2024-11-23 17:13:26 +0300

    add projects constant

---
+++ src/constants/projects.ts
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    imageSrc: `${import.meta.env.SUPABASE_STORAGE_URL}/e-message.png`,
+    title: 'E-Messenge',
+    viewProjectLink: 'https://e-messenger.vercel.app/',
+    viewCodeLink: 'https://github.com/social-tree/E-Messenger',
+  },
+  {
+    imageSrc: `${import.meta.env.SUPABASE_STORAGE_URL}/Fcets0Y.png`,
+    title: 'Potea',
+    viewProjectLink: 'https://github.com/social-tree/Potea',
+    viewCodeLink: 'https://github.com/social-tree/Potea',
+  },
+  {
+    imageSrc: `${import.meta.env.SUPABASE_STORAGE_URL}/xRg7Z68.jpeg`,
+    title: 'Dev Links',
+    viewProjectLink: 'https://github.com/social-tree/Link-sharing-app',
+    viewCodeLink: 'https://link-sharing-app-tau.vercel.app/',
+  },
+  {
+    imageSrc: `${import.meta.env.SUPABASE_STORAGE_URL}/RYNSC32.png`,
+    title: 'Astro commerce',
+    viewProjectLink: 'https://github.com/Whitelistedd/astro-ecommerce',
+    viewCodeLink: 'https://astro-ecom.vercel.app/',
+  },
+]
