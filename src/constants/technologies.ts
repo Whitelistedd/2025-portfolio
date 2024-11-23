c47414e1b7ce0be73d3c7eeed0cd2b9e683cad79
Whitelistedd <geforcexxy@gmail.com>
2024-11-23 22:44:27 +0300

    edit Navbar, HorizontalScroller, Project. add technologies

---
+++ src/constants/technologies.ts
@@ -0,0 +1,58 @@
+import Reactjs from '@/assets/img/reactjs.webp'
+import Typescript from '@/assets/img/typescript.webp'
+import Css from '@/assets/img/css.webp'
+import Git from '@/assets/img/git.webp'
+import Redux from '@/assets/img/redux.webp'
+import Sass from '@/assets/img/sass.webp'
+import Nextjs from '@/assets/img/nextjs.webp'
+import Javascript from '@/assets/img/javascript.webp'
+import Nodejs from '@/assets/img/nodejs.webp'
+import Mongodb from '@/assets/img/mongodb.webp'
+import Sanity from '@/assets/img/sanity.webp'
+
+export const technologies = [
+  {
+    name: 'ReactJS',
+    src: Reactjs.src,
+  },
+  {
+    name: 'Typescript',
+    src: Typescript.src,
+  },
+  {
+    name: 'Css',
+    src: Css.src,
+  },
+  {
+    name: 'Git',
+    src: Git.src,
+  },
+  {
+    name: 'Redux',
+    src: Redux.src,
+  },
+  {
+    name: 'Sass',
+    src: Sass.src,
+  },
+  {
+    name: 'Nextjs',
+    src: Nextjs.src,
+  },
+  {
+    name: 'Javascript',
+    src: Javascript.src,
+  },
+  {
+    name: 'Nodejs',
+    src: Nodejs.src,
+  },
+  {
+    name: 'Mongodb',
+    src: Mongodb.src,
+  },
+  {
+    name: 'Sanity',
+    src: Sanity.src,
+  },
+]
